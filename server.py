6282766537b9e8b4dbdf4f7f8bd0d95dd22d7580
Maria <mariakmoy@gmail.com>
2016-10-31 19:47:26 +0000

    Added login modal functionality

--- server.py
+++ server.py
@@ -10,10 +10,10 @@ app.secret_key = "I<3Food!"
 def show_home():
     """Display homepage"""
 
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/login')
+@app.route('/login')  # currently using modal window instead of route...
 def show_login():
     """Show login form"""
 
@@ -24,7 +24,14 @@ def show_login():
 def process_login():
     """Process login data. Add user_id to session"""
 
-    pass
+    email = request.form.get('email')
+    password = request.form.get('password')  # use encrypted hash later
+
+    session[email] = password  # if i am using user's email as a user id...
+
+    flash("Email: {} | Password: {}".format(email, password))
+
+    return redirect("/products")
 
 
 @app.route('/register')
@@ -45,21 +52,21 @@ def process_registration():
 def show_products():
     """Query database for product list & display results"""
 
-    pass
+    return render_template("products.html")
 
 
 @app.route('/products/<int:product_id>')
 def show_product_page(product_id):
     """Query database for product info and display results"""
 
-    pass
+    return render_template("product_page.html")
 
 
 @app.route('/cart')
 def show_cart():
     """Query session for cart contents and display results"""
 
-    pass
+    return render_template("cart.html")
 
 
 @app.route('/cart', methods=['POST'])
@@ -69,3 +76,11 @@ def process_order():
     pass
 
 
+if __name__ == "__main__":
+    # Change app.debug to False before launch
+    app.debug = True
+
+    # Use the DebugToolbar
+    # DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
