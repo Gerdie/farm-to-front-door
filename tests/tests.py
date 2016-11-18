c16f2dd859f7f5583fbd7013520fcf549d069ff6
Maria <mariakmoy@gmail.com>
2016-11-18 04:05:10 +0000

    Adding test coverage to front page, product listings

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from server import app
 
+
 class FlaskTests(TestCase):
 
     def setUp(self):
@@ -12,14 +13,14 @@ class FlaskTests(TestCase):
         connect_to_db(app, "postgresql:///testdb")
 
         # Create tables and add sample data
-        db.create_all()
+        # db.create_all()
         example_data()
 
     def tearDown(self):
         """For after test."""
 
         db.session.close()
-        db.drop_all()
+        # db.drop_all()
 
     def test_register(self):
         """Register test user"""
@@ -33,8 +34,33 @@ class FlaskTests(TestCase):
         self.assertIn("Registration successful!", result.data)
 
     def test_login(self):
+        """Test login functionality"""
+
         result = self.client.post("/login",
                                   data={"email": "jane@gmail.com", "password": "123"},
                                   follow_redirects=True)
         self.assertIn("Login successful!", result.data)
+        self.assertIn("Account", result.data)
+        self.assertIn("Log Out", result.data)
 
+    def test_logout(self):
+        """Test logout functionality"""
+
+        result = self.client.get("/logout", follow_redirects=True)
+
+        self.assertNotIn("Account", result.data)
+        self.assertIn("Login", result.data)
+
+    def test_frontpage(self):
+        """Test static frontpage"""
+
+        result = self.client.get("/")
+
+        self.assertIn("Enter", result.data)
+
+    def test_all_products(self):
+        """Test products listings"""
+
+        result = self.client.get("/products")
+
+        self.assertIn("Add to Cart</button>", result.data)
